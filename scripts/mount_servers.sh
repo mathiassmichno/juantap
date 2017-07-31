13aca87891c2e45220fef1a23c8ce3c91ae4f0dc
Mathias Sass Michno <mathiassmichno@users.noreply.github.com>
2017-07-31 13:33:21 +0200

    Delete mount_servers.sh

--- scripts/mount_servers.sh
+++
@@ -1,5 +0,0 @@
-for i in `seq -w 00 7`
-do
-    echo Mounting server $i
-    sudo mount -t overlay -o "lowerdir=$HOME/lower,upperdir=$HOME/servers/$i/.upper,workdir=$HOME/servers/$i/.work" overlay "$HOME/servers/$i/server"
-done
